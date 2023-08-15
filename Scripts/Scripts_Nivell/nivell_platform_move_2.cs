1e671cd59593c1e565ddb551744487a993586524
Beltrán <123870745+beltran-v-dev@users.noreply.github.com>
2023-08-15 16:45:13 +0200

    Update nivell_platform_move_2.cs

--- Scripts/Scripts_Nivell/nivell_platform_move_2.cs
+++ Scripts/Scripts_Nivell/nivell_platform_move_2.cs
@@ -1,43 +1,43 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class nivell_platform_move_2 : MonoBehaviour
 {
-    public Transform from;
-    public Transform to;
+    public Transform from;   // The starting point of the platform's movement
+    public Transform to;     // The ending point of the platform's movement
 
-    public GameObject player;
+    public GameObject player;    // Reference to the player object that interacts with the platform
 
-    private Rigidbody2D rb2dPlayer;
+    private Rigidbody2D rb2dPlayer;    // Rigidbody2D component of the player
 
-    public float speed = 0.5f;
+    public float speed = 0.5f;   // The speed at which the platform moves
 
-    private Vector2 posFixedStart;
-    private Vector2 startPosition;
-    private Vector2 endPosition;
+    private Vector2 posFixedStart;    // A fixed starting position to ensure accuracy
+    private Vector2 startPosition;    // The current starting position
+    private Vector2 endPosition;      // The ending position
 
     private void Start()
     {
-        to.parent = null;
+        to.parent = null;   // Detach 'to' from its parent to ensure proper movement
 
         startPosition = from.transform.position;
         endPosition = to.transform.position;
         posFixedStart = startPosition;
 
-        rb2dPlayer = player.GetComponent<Rigidbody2D>();
+        rb2dPlayer = player.GetComponent<Rigidbody2D>();   // Get the Rigidbody2D component of the player
     }
 
-    private void Update()
-    {
-    }
+
 
     private void FixedUpdate()
     {
+        // Move the platform towards 'to' position at a constant speed
         transform.position = Vector2.MoveTowards(transform.position, to.position, speed * Time.deltaTime);
 
         startPosition = from.position;
 
+        // Check if the platform has reached its destination, and reset the destination accordingly
         if (startPosition.x == endPosition.x)
         {
             to.position = posFixedStart;
@@ -45,30 +45,31 @@ public class nivell_platform_move_2 : MonoBehaviour
         else if (startPosition.x == to.position.x)
         {
             to.position = endPosition;
-
             // Debug.Log(transform.position.x);
-        }
-
-        void OnDrawGizmosSelected()
-        {
-            if (from != null && to != null)
-            {
-                Gizmos.color = Color.cyan;
-                Gizmos.DrawLine(from.position, to.position);
-                Gizmos.DrawSphere(from.position, 0.15f);
-                Gizmos.DrawSphere(to.position, 0.15f);
-            }
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collisionsEntery)
+    private void OnDrawGizmosSelected()
     {
-       // rb2dPlayer.velocity = new Vector3(0f, 0f, 0f);
-        player.transform.parent = collisionsEntery.transform;
+        // Draw a visual representation of the platform's movement path in the Scene view
+        if (from != null && to != null)
+        {
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawLine(from.position, to.position);
+            Gizmos.DrawSphere(from.position, 0.15f);
+            Gizmos.DrawSphere(to.position, 0.15f);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collisionsEnter)
+    {
+        // Set the player's parent to the platform when collision occurs (e.g., player steps onto platform)
+        player.transform.parent = collisionsEnter.transform;
     }
 
     private void OnCollisionExit2D(Collision2D collisionExit)
     {
+        // Remove the player's parent when they leave the platform
         player.transform.parent = null;
     }
 }
