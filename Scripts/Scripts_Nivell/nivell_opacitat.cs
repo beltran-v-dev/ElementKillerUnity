2446c4aa21ae3569be689aa8c1733586630abbeb
Beltrán <123870745+beltran-v-dev@users.noreply.github.com>
2023-08-15 16:42:05 +0200

    Update nivell_opacitat.cs

--- Scripts/Scripts_Nivell/nivell_opacitat.cs
+++ Scripts/Scripts_Nivell/nivell_opacitat.cs
@@ -1,37 +1,36 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class nivell_opacitat : MonoBehaviour
 {
     Color tmp, tmp2;
+
     private void Start()
     {
+        // Store the initial color of the SpriteRenderer component
         tmp = this.GetComponent<SpriteRenderer>().color;
         tmp.a = 0.5f;
 
-        tmp2.r = 255f;
-        tmp2.g = 255f;
-        tmp2.b = 255f;
-        tmp2.a = 255f;
+        // Define a color that will fully restore the opacity (alpha) of the SpriteRenderer
+        tmp2 = new Color(1f, 1f, 1f, 1f);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
+            // Set the SpriteRenderer's color to the semi-transparent color when the player enters the trigger
             this.GetComponent<SpriteRenderer>().color = tmp;
         }
-        
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
+            // Restore the SpriteRenderer's color to full opacity when the player exits the trigger
             this.GetComponent<SpriteRenderer>().color = tmp2;
         }
-
     }
 }
-
