fb52b2a8b78d0ea1221f045be1d791ffde27b3f1
Beltrán <123870745+beltran-v-dev@users.noreply.github.com>
2023-08-15 16:27:57 +0200

    Update player_moviment.cs

--- Scripts/Scripts_Player/player_moviment.cs
+++ Scripts/Scripts_Player/player_moviment.cs
@@ -1,38 +1,39 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class player_moviment : MonoBehaviour
 {
-    public player_control controller;
+    public player_control controller;  // Reference to the player control script.
 
-    private float horizontalMove = 0f;
+    private float horizontalMove = 0f;  // Stores the horizontal movement input value.
 
-    public float runSpeed = 40f;
+    public float runSpeed = 40f;  // The speed at which the player character moves.
 
-    private bool jump = false;
+    private bool jump = false;  // Flag to indicate if the player wants to jump.
 
-    private int numJump = 0;
+    private int numJump = 0;  // Number of times the player has jumped.
 
     private void Start()
     {
+        // Initialization code that runs when the script starts.
     }
 
     private void Update()
     {
-        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;  // Get the horizontal input for movement.
 
         if (Input.GetButtonDown("Jump"))
         {
-            jump = true;
+            jump = true;  // Detect if the player pressed the jump button.
         }
     }
 
-    //Moviemnt del personatge en fixedUpdate
+    // Movement of the character in FixedUpdate.
     private void FixedUpdate()
     {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);  // Move the player using the controller script.
 
-        jump = false;
+        jump = false;  // Reset the jump flag.
     }
 }
