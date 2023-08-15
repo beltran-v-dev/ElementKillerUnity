b0da05f57d398b72b092afa15146041e0482711c
Beltrán <123870745+beltran-v-dev@users.noreply.github.com>
2023-08-15 16:33:50 +0200

    Update player_vida.cs

--- Scripts/Scripts_Player/player_vida.cs
+++ Scripts/Scripts_Player/player_vida.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
@@ -19,45 +19,52 @@ public class player_vida : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
+        // Get the SpriteRenderer component of the player object
         m_SpriteRenderer = GetComponent<SpriteRenderer>();
         animacio = GetComponent<Animator>();
 
+        // Set the initial material color to black
         material.SetColor("_ToColor", Color.black);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        // Find the Shoot object and get its player_shoot component
         GameObject element = GameObject.Find("Shoot");
         player_shoot tipusElement = element.GetComponent<player_shoot>();
 
         if (collision.gameObject.tag == "Enemic" || collision.gameObject.tag == "Enemic_2" || collision.gameObject.tag == "Enemic_3")
         {
-            //m_SpriteRenderer.sprite = Resources.Load<Sprite>("player_base");
+            // Change the player's sprite color and reset the element type
             animacio.SetInteger("color", 0);
-
             tipusElement.element = 0;
 
+            // Destroy the enemy that collided with the player
             Destroy(collision.gameObject);
 
+            // Decrease player's life and update the life text
             vida--;
-
             vidaText.SetText("" + vida);
 
+            // Change the screen color to black
             material.SetColor("_ToColor", Color.black);
 
+            // If player's life is zero, show "You Died!" message and pause the game
             if (vida == 0)
             {
-                Debug.Log("Has mort!");
+                Debug.Log("You died!");
                 canvasYouDied.SetActive(true);
-                Time.timeScale = 0f;
+                Time.timeScale = 0f; // Pause the game
             }
         }
 
+        // If the player collides with a "Nivell" object, make it a child of that object
         if (collision.gameObject.tag == "Nivell")
         {
             transform.parent = collision.transform;
         }
 
+        // If the player collides with a "cor" object, increase their life
         if (collision.gameObject.tag == "cor")
         {
             vida = vida + 1;
@@ -69,20 +76,18 @@ public class player_vida : MonoBehaviour
 
     private void Update()
     {
+        // Change the screen color based on the pressed keys
         if (Input.GetKeyDown(KeyCode.Alpha1))
-
         {
             material.SetColor("_ToColor", Color.red);
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha2))
-
         {
             material.SetColor("_ToColor", Color.green);
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha3))
-
         {
             material.SetColor("_ToColor", Color.blue);
         }
