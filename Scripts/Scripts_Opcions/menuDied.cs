b4f8c5e35ebb49590958f52b4f1cd85f500b4769
Beltrán <123870745+beltran-v-dev@users.noreply.github.com>
2023-08-15 16:47:19 +0200

    Update menuDied.cs

--- Scripts/Scripts_Opcions/menuDied.cs
+++ Scripts/Scripts_Opcions/menuDied.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -10,10 +10,10 @@ public class menuDied : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.anyKeyDown)
+        if (Input.anyKeyDown)
         {
-            Time.timeScale = 1f;
-            SceneManager.LoadScene("ElementKiller1.2");
+            Time.timeScale = 1f;   // Reset the time scale to normal speed
+            SceneManager.LoadScene("ElementKiller1.2");   // Load the specified scene when any key is pressed
         }
     }
 }
