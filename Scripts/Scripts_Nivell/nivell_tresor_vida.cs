db669156bccfde324ee5dcebcf4feec114bf8ead
Beltrán <123870745+beltran-v-dev@users.noreply.github.com>
2023-08-15 16:45:58 +0200

    Update nivell_tresor_vida.cs

--- Scripts/Scripts_Nivell/nivell_tresor_vida.cs
+++ Scripts/Scripts_Nivell/nivell_tresor_vida.cs
@@ -1,23 +1,21 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class nivell_tresor_vida : MonoBehaviour
 {
-    private SpriteRenderer Sprite;
+    private SpriteRenderer Sprite;    // The SpriteRenderer component of the treasure
 
     private void Start()
     {
-        Sprite = this.GetComponent<SpriteRenderer>();
+        Sprite = this.GetComponent<SpriteRenderer>();   // Get the SpriteRenderer component of the treasure
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            Destroy(this.gameObject);
-
+            Destroy(this.gameObject);   // Destroy the treasure when the player touches it
         }
-
     }
 }
