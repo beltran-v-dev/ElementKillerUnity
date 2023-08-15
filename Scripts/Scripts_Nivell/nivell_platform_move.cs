c0436a5655794b33a7929136395f41e1be655477
Beltrán <123870745+beltran-v-dev@users.noreply.github.com>
2023-08-15 16:43:55 +0200

    Update nivell_platform_move.cs

--- Scripts/Scripts_Nivell/nivell_platform_move.cs
+++ Scripts/Scripts_Nivell/nivell_platform_move.cs
@@ -1,37 +1,40 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class nivell_platform_move : MonoBehaviour
 {
-    public Transform from;
-    public Transform to;
+    public Transform From;   // The starting point of the platform's movement
+    public Transform to;     // The ending point of the platform's movement
 
-    public float speed = 0.5f;
+    public float speed = 0.5f;   // The speed at which the platform moves
 
-    private Vector2 posFixedStart;
-    private Vector2 startPosition;
-    private Vector2 endPosition;
+    private Vector2 posFixedStart;    // A fixed starting position to ensure accuracy
+    private Vector2 startPosition;    // The current starting position
+    private Vector2 endPosition;      // The ending position
 
     private void Start()
     {
-        to.parent = null;
+        to.parent = null;   // Detach 'to' from its parent to ensure proper movement
 
-        startPosition = from.transform.position;
+        startPosition = From.transform.position;
         endPosition = to.transform.position;
         posFixedStart = startPosition;
     }
 
     private void Update()
     {
+        // This section can be used for input or non-physics updates
     }
 
     private void FixedUpdate()
     {
+        // Move the platform towards 'to' position at a constant speed
         transform.position = Vector2.MoveTowards(transform.position, to.position, speed * Time.deltaTime);
 
-        startPosition = from.position;
+        startPosition = From.position;
 
+        // Check if the platform has reached its destination, and reset the destination accordingly
         if (startPosition.x == endPosition.x)
         {
             to.position = posFixedStart;
@@ -46,11 +49,12 @@ public class nivell_platform_move : MonoBehaviour
 
     private void OnDrawGizmosSelected()
     {
-        if (from != null && to != null)
+        // Draw a visual representation of the platform's movement path in the Scene view
+        if (From != null && to != null)
         {
             Gizmos.color = Color.cyan;
-            Gizmos.DrawLine(from.position, to.position);
-            Gizmos.DrawSphere(from.position, 0.15f);
+            Gizmos.DrawLine(From.position, to.position);
+            Gizmos.DrawSphere(From.position, 0.15f);
             Gizmos.DrawSphere(to.position, 0.15f);
         }
     }
