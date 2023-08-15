a9a5d65b893964bd9d7e83e33a8efd4644cece27
Beltrán <123870745+beltran-v-dev@users.noreply.github.com>
2023-08-15 16:50:02 +0200

    Update options_camera_seguir.cs

--- Scripts/Scripts_Opcions/options_camera_seguir.cs
+++ Scripts/Scripts_Opcions/options_camera_seguir.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,15 +7,11 @@ public class options_camera_seguir : MonoBehaviour
     public GameObject player;
     public Vector3 offset;
         
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
+        // Set the camera's position to follow the player with the specified offset
         transform.position = new Vector3(player.transform.position.x + offset.x, player.transform.position.y + offset.y, offset.z);
     }
 }
