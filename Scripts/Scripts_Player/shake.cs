c40705c978180a2e86315fb2f9f34da226fd09fa
Beltrán <123870745+beltran-v-dev@users.noreply.github.com>
2023-08-15 16:34:52 +0200

    Update shake.cs

--- Scripts/Scripts_Player/shake.cs
+++ Scripts/Scripts_Player/shake.cs
@@ -1,37 +1,37 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
-using UnityEngine.Events;
 
 public class shake : MonoBehaviour
 {
-    public float ShakeDuration = 0.3f;
-    public float ShakeAmplitude = 1.2f;
-    public float ShakeFrequency = 2.0f;
+    public float ShakeDuration = 0.3f; // Duration of the shake effect
+    public float ShakeAmplitude = 1.2f; // Intensity of the shake
+    public float ShakeFrequency = 2.0f; // Frequency of the shake
 
-    private float ShakeElapsedTime = 0f;
+    private float ShakeElapsedTime = 0f; // Timer for the shake effect
 
-    public CinemachineVirtualCamera VirtualCamera;
-
-    private CinemachineBasicMultiChannelPerlin virtualCameraNoise;
+    public CinemachineVirtualCamera VirtualCamera; // Reference to the virtual camera
+    private CinemachineBasicMultiChannelPerlin virtualCameraNoise; // Noise module for camera shaking
 
     private void Start()
     {
         if (VirtualCamera != null)
-            virtualCameraNoise = VirtualCamera.GetCinemachineComponent<Cinemachine.CinemachineBasicMultiChannelPerlin>();
+            virtualCameraNoise = VirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Enemic" || collision.gameObject.tag == "Enemic_2" || collision.gameObject.tag == "Enemic_3")
         {
+            // Trigger the camera shake when colliding with an enemy
             shaking();
         }
     }
 
     private void Update()
     {
+        // Update camera shake effect if needed
         if (VirtualCamera != null && virtualCameraNoise != null)
         {
             if (ShakeElapsedTime > 0)
@@ -43,6 +43,7 @@ public class shake : MonoBehaviour
             }
             else
             {
+                // Reset camera noise when shake is done
                 virtualCameraNoise.m_AmplitudeGain = 0f;
                 ShakeElapsedTime = 0f;
             }
@@ -51,6 +52,7 @@ public class shake : MonoBehaviour
 
     public void shaking()
     {
+        // Trigger the camera shake effect
         ShakeElapsedTime = ShakeDuration;
     }
 }
